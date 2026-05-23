4632473fae8bb6b8209fae7529c376381a8de1b4
Inderpal Singh <inderpalsmutta@gmail.com>
2026-05-23 17:35:26 +0530

    Initial commit

---
+++ src/test/java/com/javasampleproject/JavaSampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javasampleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
